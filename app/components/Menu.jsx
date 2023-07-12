d13d8d0c4338fb45de54b214b8cc4b04855009cf
abdulhadi240 <ah912425@gmail.com>
2023-07-12 22:22:24 +0500

    third

--- app/components/Menu.jsx
+++ app/components/Menu.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Menu = ({border , background , color , hover , margin , active,hide}) => {
   return (
     <div className={`h-10   py-auto sm:w-full ${background ? 'bg-[#98ca3e]' : ''}  mt-${margin} lg:block ${hide?'hidden' : 'block'}`}>
-        <div className={`  grid grid-cols-3 text-xs sm:text-base sm:w-full sm:flex sm:justify-between text-center   ${color?`text-white` : 'text-gray-400'} overflow-hidden`}>
+        <div className={` -ml-8 grid grid-cols-3  text-[10px] sm:text-base sm:w-full sm:flex sm:justify-between text-center   ${color?`text-white` : 'text-gray-400'} overflow-hidden`}>
             <Link  href={'./'}><Name Text={'HOME'} border={border} hover={border}  TextHover={hover} active={active}/></Link>
             <Link href={'./AboutUs'}><Name Text={'ABOUT US'} border={border} hover={border} TextHover={hover} active={active}/></Link>
             <Link href={'./'}><Name Text={'PORDUCTS'} border={border} hover={border} TextHover={hover} active={active}/></Link>
